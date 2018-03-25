6f0482aaa512c4496a1eb9cec23aaad3291c93c3
sofiamv <League@AdministorsiMac.attlocal.net>
2018-03-25 12:23:00 -0700

    MAZE AND GAME!
    I opened the Ninja Star Program, made my Quiz Game better, and made a
    scary maze

--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -6,33 +6,96 @@ public class QuizGame {
 
 	public static void main(String[] args) {
 
-		// 1. Create a variable to hold the user's score
 		int score = 0;
-		// 2. Ask the user a question
-		String answer = JOptionPane.showInputDialog("What is life?");
-		// 3. Use an if statement to check if their answer is correct
-		if (answer.equals("LIFE")) {
-			score++;
-		}
-		// 4. if the user's answer is correct
+		
+		String answer = JOptionPane.showInputDialog("What is life? "
+				+ " a. poop"
+				+ " b. very poopy"
+				+ " c. LIFE"
+				+ " d. all of the above ");
 
-		// -- add one to their score
-
-		// 5. Create more questions by repeating steps 1, 2, and 3 below.
-		answer = JOptionPane.showInputDialog("What is 5+2?");
-		if (answer.equals("7")) {
+		if (answer.equals("c")) {
 			score++;
 		}
-		answer = JOptionPane.showInputDialog("What is 5 to the power of 3?");
-		if (answer.equals("125")) {
-			score++;
-		}
-		answer = JOptionPane.showInputDialog("What is 50+50");
-		if (answer.equals("100")) {
-			score++;
+		else {
+			JOptionPane.showMessageDialog(null,"Correct Answer = c. LIFE");
 		}
 
-		// 6. After all the questions have been asked, print the user's score
+		answer = JOptionPane.showInputDialog("What is 5+2?"
+				+ " a. Why are you asking me?"
+				+ " b. 52"
+				+ " c. 9"
+				+ " d. 7");
+		if (answer.equals("d")) {
+			score++;
+
+		}
+		else {
+			JOptionPane.showMessageDialog(null,"Correct Answer = d. 7");
+		}
+
+		answer = JOptionPane.showInputDialog("What is 5 to the power of 3?"
+				+ " a. 15"
+				+ " b. 125"
+				+ " c. 0"
+				+ " d. I DON'T WANT TO DO MATH!!");
+		if (answer.equals("b")) {
+			score++;
+		}
+		else {
+			JOptionPane.showMessageDialog(null,"Correct Answer = b. 125");
+		}
+
+		answer = JOptionPane.showInputDialog("What is 50+50"
+				+ " a. 100"
+				+ " b. 5050"
+				+ " c. 0"
+				+ " d. 2500");
+		if (answer.equals("a")) {
+			score++;
+		}
+		else {
+			JOptionPane.showMessageDialog(null,"Correct Answer = a. 100");
+		}
+
+		
+		answer = JOptionPane.showInputDialog("What am I?"
+				+ " a. Computer"
+				+ " b. A Thing That can Multitask"
+				+ " c. A Machine That Can type Back  To You"
+				+ " d. all of the above");
+				
+		if (answer.equals("d")) {
+			score++;
+		}
+		else {
+			JOptionPane.showMessageDialog(null,"Correct Answer = d. all of the above");
+		}
+
+
+		answer = JOptionPane.showInputDialog("Who was the second president of the United States of America?"
+				+ " a. IDK"
+				+ " b. Obama"
+				+ " c. Donald Duck"
+				+ " d. John Adams");
+		if (answer.equals("d")) {
+			score++;
+		}
+		else {
+			JOptionPane.showMessageDialog(null,"Correct Answer = d. John Adams");
+		}
+
+		answer = JOptionPane.showInputDialog("What is the capital of Venusvela?"
+				+ " a. The United States of America"
+				+ " b. A place"
+				+ " c. Caracas"
+				+ " d. Alabama");
+		if (answer.equals("c")) {
+			score++;
+		}
+		else {
+			JOptionPane.showMessageDialog(null,"Correct Answer = c. Caracas");
+		}
 		JOptionPane.showMessageDialog(null, score);
 	}
 }
